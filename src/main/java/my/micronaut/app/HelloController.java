a3e7340b5784344aa89be317a17d6c9d93940244
karthik <karthik.mk@spurtreetech.com>
2025-02-28 15:23:56 +0530

    Clean code for controller file, test commit

--- src/main/java/my/micronaut/app/HelloController.java
+++ src/main/java/my/micronaut/app/HelloController.java
@@ -4,6 +4,8 @@ import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.HttpStatus;
 
+
+// Testo controller with get request 
 @Controller("/hello")
 public class HelloController {
     @Get("/") // Handles GET requests to /hello
@@ -11,25 +13,3 @@ public class HelloController {
         return "Hello, KArthik!";
     }
 }
-
-
-// package my.micronaut.app;
-
-// import io.micronaut.http.MediaType;
-// import io.micronaut.http.annotation.Controller;
-// import io.micronaut.http.annotation.Get;
-
-
-// @Controller("/hello")
-// public class HelloController {
-
-//     // this empty constructor is not
-//     // needed, but isn't a problem...
-//     HelloController() {
-//     }
-
-//     @Get(value = "/", produces = MediaType.TEXT_PLAIN)
-//     String getTest() {
-//         return "some string";
-//     }
-// }
