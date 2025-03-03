c6845bd4ae6a809350ecbcddefc50e7739d77c9d
karthik <karthik.mk@spurtreetech.com>
2025-03-03 13:11:00 +0530

    Test Hello Contoller

--- src/main/java/my/micronaut/app/HelloController.java
+++ src/main/java/my/micronaut/app/HelloController.java
@@ -2,14 +2,13 @@ package my.micronaut.app;
 
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import io.micronaut.http.HttpStatus;
 
 
-// Testo controller with get request 
-@Controller("/hello")
+@Controller
 public class HelloController {
+
     @Get("/") // Handles GET requests to /hello
-    public String sayHello() {
-        return "Hello, KArthik!";
-    }
+            public String sayHello() {
+                return "Hello Micronaout!";
+            }
 }
